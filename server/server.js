116cf13a1c989ba44a0e6860bdc93603fe482252
Tu Nguyen <tus@Tus-MacBook-Pro.local>
2024-02-17 18:03:05 -0600

    Define PORT in server.js file instead of .env

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const PORT = 4000;
 //Require library for PORT
-const dotevn = require('dotenv');
+// const dotevn = require('dotenv');
 //Invoke config method
-dotevn.config();
+// dotevn.config();
 
 app.use(cors());
 app.use(express.static(`${__dirname}/public`))
@@ -15,5 +16,5 @@ app.get('/api/cat', (req, res) => {
     res.status(200).send('Smelly Cat')
 });
 
-const { PORT } = process.env;
+// const { PORT } = process.env;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
